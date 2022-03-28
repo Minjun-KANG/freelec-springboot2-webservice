947af93ab3212d55610f33712f62f921672a8bac
Minjun-KANG <inventer@kakao.com>
2022-03-28 12:06:58 +0900

    non lombok version commit

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // JSON을 반환하는 컨트롤러로 만들어줌 메소드마다 Response body 안써도됨.
 public class HelloController {
     @GetMapping("/hello") // Get 요청을 받을 수 있음
-    public String hello()
-    {
+    public String hello() {
         return "Hello";
     }
 }
