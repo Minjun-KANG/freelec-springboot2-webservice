67a22466b3f6c54b03f1cd1e21515ac19b03414c
Minjun-KANG <inventer@kakao.com>
2022-03-29 17:34:15 +0900

    Adding OAuth Naver, Do not use Annotations

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -9,11 +9,11 @@ import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
-    private Map<String, Object> attributes;
-    private String nameAttributeKey;
-    private String name;
-    private String email;
-    private String picture;
+    private final Map<String, Object> attributes;
+    private final String nameAttributeKey;
+    private final String name;
+    private final String email;
+    private final String picture;
 
     @Builder
     private OAuthAttributes(Map<String, Object> attributes,
@@ -27,7 +27,22 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -1,8 +1,20 @@
 package com.jojoldu.book.springboot.config.auth.dto;
 
 import com.jojoldu.book.springboot.domain.user.User;
+import lombok.Getter;
 
-public class SessionUser {
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private final String name;
+    private final String email;
+    private final String picture;
+
     public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getEmail();
+
     }
 }
